50184f619aa118f76c0643451f3ec3e7727b0246
nawiq7208 <ipsurbakti7208@gmail.com>
2022-07-31 01:59:28 +0800

    Update Users.js

--- backend/controllers/Users.js
+++ backend/controllers/Users.js
@@ -1,10 +1,47 @@
 import Users from "../models/UserModel.js";
+import argon2 from "argon2";
 
-export const getUsers = (req, res) => {};
+export const getUsers = async (req, res) => {
+	try {
+		const response = await Users.findAll();
+		res.status(200).json(response);
+	} catch (error) {
+		res.status(500).json({ msg: error.message });
+	}
+};
 
-export const getUserById = (req, res) => {};
+export const getUserById = async (req, res) => {
+	try {
+		const response = await Users.findOne({
+			where: {
+				uuid: req.params.id,
+			},
+		});
+		res.status(200).json(response);
+	} catch (error) {
+		res.status(500).json({ msg: error.message });
+	}
+};
 
-export const createUser = (req, res) => {};
+export const createUser = async (req, res) => {
+	const { name, email, password, confPassword, role } = req.body;
+	if (password !== confPassword)
+		return res
+			.status(400)
+			.json({ msg: "Password and Confirm Password are not match" });
+	const hashPassword = await argon2.hash(password);
+	try {
+		await Users.create({
+			name: name,
+			email: email,
+			password: hashPassword,
+			role: role,
+		});
+		res.status(201).json({ msg: "Register successfuly" });
+	} catch (error) {
+		res.status(400).json({ msg: error.message });
+	}
+};
 
 export const updateUser = (req, res) => {};
 
